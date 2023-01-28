1351993adf8b4c701f072ee78577f9205e995cd5
dangakeri <dangakeri254@gmail.com>
2023-01-28 14:33:14 +0300

    text

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,34 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  final FlutterLocalization localization = FlutterLocalization.instance;
+class MyApp extends StatefulWidget {
   MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final FlutterLocalization localization = FlutterLocalization.instance;
+
+  @override
+  void initState() {
+    localization.init(
+      mapLocales: [
+        const MapLocale('en', AppLocale.EN),
+        const MapLocale('km', AppLocale.KM),
+        const MapLocale('ja', AppLocale.JA),
+      ],
+      initLanguageCode: 'en',
+    );
+    localization.onTranslatedLanguage = _onTranslatedLanguage;
+    super.initState();
+  }
+
+  /// the setState function here is a must to add
+  void _onTranslatedLanguage(Locale? locale) {
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -47,24 +72,23 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: const <Widget>[
+            SizedBox(height: 20),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'My hometown is a beautiful place nestled in a lush landscape surrounded by stunning mountains and crystal clear rivers. Growing up here has been an incredible experience, as I have been exposed to a variety of cultures, languages, and traditions. I have also been able to explore the diverse wildlife and ecosystems that make up my continent. I am deeply connected to this place, because it is where I have spent my entire life, and where I have formed meaningful relationships with my family and friends.',
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'The people here are incredibly welcoming and friendly, and I am always met with warmth and hospitality whenever I visit. As someone who has grown up here, I am proud of the rich history, culture, and heritage that shapes our continent. I love the way that different cultures coexist and intersect here, and I am in awe of the vibrant art, music, and dance that springs from the vibrant souls of the people here',
+              style: TextStyle(fontSize: 16),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
@@ -83,4 +107,12 @@ class Lang {
     const Locale('fr'),
     #french,
   ];
+}
+
+mixin AppLocale {
+  static const String title = 'title';
+
+  static const Map<String, dynamic> EN = {title: 'Localization'};
+  static const Map<String, dynamic> KM = {title: 'ការធ្វើមូលដ្ឋានីយកម្ម'};
+  static const Map<String, dynamic> JA = {title: 'ローカリゼーション'};
 }
